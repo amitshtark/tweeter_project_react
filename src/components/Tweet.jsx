f3dd947b36a6e4be26122cd0a790f145d24815bf
Amit Shtark <amit.shtark@gmail.com>
2026-07-11 17:02:01 +0300

    Fix props usage in Tweet component to correctly reference tweet object

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -5,11 +5,11 @@ function Tweet({ tweet }) {
     return (
         <div className="tweet">
             <div className="tweet-header">
-                <span className="tweet-username">{props.userName}</span>
-                <span className="tweet-date">{props.date}</span>
+                <span className="tweet-username">{tweet.userName}</span>
+                <span className="tweet-date">{tweet.date}</span>
             </div>
 
-            <p className="tweet-content">{props.content}</p>
+            <p className="tweet-content">bla {tweet.content}</p>
         </div>
     );
 }
