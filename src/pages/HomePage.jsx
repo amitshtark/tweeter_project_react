b0fd90fccf52a9ef89bde83cf203228b634dcc0b
Amit Shtark <amit.shtark@gmail.com>
2026-07-11 13:15:07 +0300

    removed date logic from HomePage's addTweet function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
-import  CreateTweet from "./components/CreateTweet.jsx"
-import  TweetList from "./components/TweetList.jsx"
+import  CreateTweet from "..components/CreateTweet.jsx"
+import  TweetList from "..components/TweetList.jsx"
 
 
 function HomePage() {
 
     const [tweets, setTweets] = useState([]);
-    const USERNAME = "idk what to write here"
+    const USERNAME = "Amit"
 
 
 
     function addTweet(content){
         const id = Date.now();
-        const date = new Date(id).toLocaleString([], {
-            dateStyle: "short",
-            timeStyle: "short"
-        });
-        let newTweet = {id: id, content: content, date: date, userName: USERNAME}
-        setTweets([...tweets, newTweet])
+        const newTweet = {
+            id: Date.now(),
+            content: content,
+            date: new Date().toISOString(),
+            userName: USERNAME,
+        };
+        setTweets([newTweet, ...tweets])
     }
     
     
     return (
     <div>
-      <CreateTweet></CreateTweet>
-      <TweetList></TweetList>
+      <CreateTweet addTweet = {addTweet}></CreateTweet>
+      <TweetList tweets = {tweets}></TweetList>
     </div>
     );
 }
