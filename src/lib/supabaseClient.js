cd757d725e86cef846b423028fa65b4900351de7
Amit Shtark <amit.shtark@gmail.com>
2026-07-23 21:19:17 +0300

    Fix Supabase URL and key in supabaseClient.js for correct endpoint configuration

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://amitshtark.github.io/tweeter_project_react/#/profile";
-const supabaseKey = "https://jzuitxcudononvulzkrc.supabase.co/rest/v1";
+const supabaseUrl = "https://amitshtark.github.io/tweeter_project_react";
+const supabaseKey = "https://jzuitxcudononvulzkrc.supabase.co";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
